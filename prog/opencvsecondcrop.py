b5f2cb79afeef921a9c9d6c64da5249731ccbf6e
Winnguyen1511 <khoanguyen1507dn@gmail.com>
2020-05-17 11:40:32 +0700

    update opencv secondcrop

--- prog/opencvsecondcrop.py
+++ prog/opencvsecondcrop.py
@@ -1,3 +1,6 @@
+from darkflow.net.build import TFNet
+import tensorflow as tf
+from tensorflow.keras import layers, models
 import numpy as np
 import cv2
 import imutils
@@ -9,3 +12,78 @@ import os
 import recognitionmodules as rm
 
 resources_path = 'resources/'
+
+def opencvsecondCrop(img):
+    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    thresh = cv2.adaptiveThreshold(gray,210,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY_INV,39,1)
+    edges = rm.auto_canny(thresh, sigma=0.1)
+    # edges =thresh
+    ctrs, hier = cv2.findContours(edges,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_TC89_L1)
+    # areas = [cv2.contourArea(c) for c in contours]
+    # if(len(areas)!=0):
+    #     max_index = np.argmax(areas)
+    #     cnt=contours[max_index]
+    #     x,y,w,h = cv2.boundingRect(cnt)
+    #     cv2.rectangle(thresh,(x,y),(x+w,y+h),(0,255,0),2)
+    #     cv2.imshow('border',thresh)
+    #     secondCrop = img[y:y+h,x:x+w]
+    # else: 
+    #     secondCrop = img
+    # return secondCrop
+    chosen_lst = []
+    areas = []
+    img_area = img.shape[0]*img.shape[1]
+    # count = 0
+    for i, ctr in enumerate(ctrs):
+        x,y, w, h = cv2.boundingRect(ctr)
+        roi_area = w*h
+        non_max_sup = roi_area/img_area
+        if non_max_sup > 0.75:
+            # cv2.rectangle(img,(x,y),(x+w,y+h),(90,0,255),2)
+            # count+=1
+            chosen_lst.append([x,y,w,h])
+            areas.append(roi_area)
+    # print('> Num:',count)
+    if len(chosen_lst) != 0:
+        max_index = np.argmin(areas)
+        x, y, w, h = chosen_lst[max_index]
+        cv2.rectangle(img,(x,y), (x+w, y+h), (90,0,255),2)
+        secondCrop = img[y:y+h,x:x+w]
+    else:
+        secondCrop = img
+    cv2.imshow('image:', img)
+    cv2.imshow('invert',edges)
+    return secondCrop
+
+
+def opencvCrop(image, yoloPlate):
+    prediction = yoloPlate.return_predict(image)
+    if len(prediction) > 0:
+        image = rm.firstCrop(image, prediction)
+        cv2.imshow('firstCrop',image)
+        image = opencvsecondCrop(image)
+        cv2.imshow('secondCrop',image)
+    
+
+
+def main():
+    plateOptions = {"pbLoad": 'database/protobuf/yolo-plate.pb', "metaLoad": 'database/meta/yolo-plate.meta', "gpu": 0.9}
+    yoloPlate = TFNet(plateOptions)
+    os.chdir(resources_path)
+    while True:
+        print("Please enter the image (0 or q to exit)")
+        image = input("> Input image: ")
+        if(image == '0' or image == 'q'):
+            break
+        img = cv2.imread(image)
+        start = time.time()
+        opencvCrop(img, yoloPlate)
+        stop = time.time()
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        t = stop - start
+        print(">> Time:%.2fsec "%(t))
+        print(">> FPS :%.2f"%(1/t))
+    print("Exiting...")
+if __name__ == '__main__':
+    main()
--- prog/recognitionmodules.py
+++ prog/recognitionmodules.py
@@ -24,21 +24,45 @@ def firstCrop(img, predictions):
     cv2.rectangle(img,(xtop,ytop),(xbottom,ybottom),(0,255,0),3)
     return firstCrop
     
+# def secondCrop(img):
+#     gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+#     ret,thresh = cv2.threshold(gray,127,255,0)
+#     contours,_ = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_TC89_L1)
+#     areas = [cv2.contourArea(c) for c in contours]
+#     if(len(areas)!=0):
+#         max_index = np.argmax(areas)
+#         cnt=contours[max_index]
+#         x,y,w,h = cv2.boundingRect(cnt)
+#         cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+#         secondCrop = img[y:y+h,x:x+w]
+#     else: 
+#         secondCrop = img
+#     return secondCrop
+
 def secondCrop(img):
     gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    ret,thresh = cv2.threshold(gray,127,255,0)
-    contours,_ = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_TC89_L1)
-    areas = [cv2.contourArea(c) for c in contours]
-    if(len(areas)!=0):
-        max_index = np.argmax(areas)
-        cnt=contours[max_index]
-        x,y,w,h = cv2.boundingRect(cnt)
-        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+    thresh =  cv2.adaptiveThreshold(gray,210,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY_INV,39,1)
+    edges = auto_canny(thresh, sigma=0.1)
+    ctrs,_ = cv2.findContours(edges,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_TC89_L1)
+    chosen_lst = []
+    areas = []
+    img_area = img.shape[0]*img.shape[1]
+    # count = 0
+    for i, ctr in enumerate(ctrs):
+        x,y, w, h = cv2.boundingRect(ctr)
+        roi_area = w*h
+        non_max_sup = roi_area/img_area
+        if non_max_sup > 0.70 and non_max_sup <=0.90:
+            chosen_lst.append([x,y,w,h])
+            areas.append(roi_area)
+    if len(chosen_lst) != 0:
+        max_index = np.argmin(areas)
+        x, y, w, h = chosen_lst[max_index]
+        # cv2.rectangle(img,(x,y), (x+w, y+h), (90,0,255),2)
         secondCrop = img[y:y+h,x:x+w]
-    else: 
+    else:
         secondCrop = img
     return secondCrop
-
 def auto_canny(image, sigma=0.33):
     # compute the median of the single channel pixel intensities
     v = np.median(image)
@@ -111,7 +135,7 @@ def opencvReadPlate(img, characterRecognition, show=True):
         roi_area = w*h
         non_max_sup = roi_area/img_area
 
-        if((non_max_sup >= 0.02) and (non_max_sup < 0.09)):
+        if((non_max_sup >= 0.015) and (non_max_sup < 0.09)):
             # if ((h>1.2*w) and (3*w>=h)):
             if(h > w):
                 # char = img[y:y+h,x:x+w]
@@ -151,8 +175,8 @@ def opencvReadPlate(img, characterRecognition, show=True):
 
 def cnnCharRecognition(img,characterRecognition):
     dictionary = {0:'0', 1:'1', 2 :'2', 3:'3', 4:'4', 5:'5', 6:'6', 7:'7', 8:'8', 9:'9', 10:'A',
-    11:'B', 12:'C', 13:'D', 14:'E', 15:'F', 16:'G', 17:'H', 18:'I', 19:'J', 20:'K',
-    21:'L', 22:'M', 23:'N', 24:'P', 25:'Q', 26:'R', 27:'S', 28:'T', 29:'U',
+    11:'B', 12:'C', 13:'D', 14:'E', 15:'F', 16:'G', 17:'H', 18:'1', 19:'J', 20:'K',
+    21:'L', 22:'M', 23:'N', 24:'P', 25:'Q', 26:'R', 27:'S', 28:'1', 29:'U',
     30:'V', 31:'W', 32:'X', 33:'Y', 34:'Z'}
     # print(img.shape)
     blackAndWhiteChar=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
