b046d80a56f45010a69e0fa1202fff523a38e9da
Winnguyen1511 <48133474+Winnguyen1511@users.noreply.github.com>
2020-05-22 12:05:12 +0700

    Add files via upload

--- prog/recognitionmodules.py
+++ prog/recognitionmodules.py
@@ -13,54 +13,41 @@ MIN_HEIGHT = 80
 MAX_WIDTH = 600
 MAX_HEIGHT = 160
 
-SECOND_CROP_LOWLIM = 0.7
-SECOND_CROP_HIGHLIM = 0.9
 
-SEGMENT_LOWLIM = 0.02
-SEGMENT_HIGHLIM = 0.09
-
-INVERT_LIM = 200
-
-def firstCrop(img, predictions, show=True):
+def firstCrop(img, predictions):
     predictions.sort(key=lambda x: x.get('confidence'))
     xtop = predictions[-1].get('topleft').get('x')
     ytop = predictions[-1].get('topleft').get('y')
     xbottom = predictions[-1].get('bottomright').get('x')
     ybottom = predictions[-1].get('bottomright').get('y')
     firstCrop = img[ytop:ybottom, xtop:xbottom]
-    if show == True:
-        tmpImg = img.copy()
-        cv2.rectangle(tmpImg,(xtop,ytop),(xbottom,ybottom),(0,255,0),3)
-        imsize = tmpImg.shape
-        w = imsize[1]
-        h = imsize[0]
-        if w < MIN_WIDTH or h < MIN_HEIGHT:
-            w_scale = math.ceil(MIN_WIDTH / w)
-            h_scale = math.ceil(MIN_HEIGHT / h)
-            scale = max(w_scale, h_scale)
-            w = w * scale
-            h = h * scale
-        if w > MAX_WIDTH or h > MAX_HEIGHT:
-            w_scale = math.ceil(w / MAX_WIDTH)
-            h_scale = math.ceil(h / MAX_HEIGHT)
-            scale = min(w_scale, h_scale)
-            w = math.floor(w / scale)
-            h = math.floor(h / scale)
-        tmpImg = cv2.resize(tmpImg, (w, h), interpolation=cv2.INTER_CUBIC)
-        cv2.imshow('Detection',tmpImg)
+    cv2.rectangle(img,(xtop,ytop),(xbottom,ybottom),(0,255,0),3)
     return firstCrop
     
+# def secondCrop(img):
+#     gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+#     ret,thresh = cv2.threshold(gray,127,255,0)
+#     contours,_ = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_TC89_L1)
+#     areas = [cv2.contourArea(c) for c in contours]
+#     if(len(areas)!=0):
+#         max_index = np.argmax(areas)
+#         cnt=contours[max_index]
+#         x,y,w,h = cv2.boundingRect(cnt)
+#         cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+#         secondCrop = img[y:y+h,x:x+w]
+#     else: 
+#         secondCrop = img
+#     return secondCrop
 
-def secondCrop(img, show=True):
+def secondCrop(img):
     gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    thresh =  cv2.adaptiveThreshold(gray,INVERT_LIM,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY_INV,39,1)
+    thresh =  cv2.adaptiveThreshold(gray,210,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY_INV,39,1)
     edges = auto_canny(thresh, sigma=0.1)
-    
-    cv_version = cv2.__version__
-    if cv_version == '4.2.0':
+    version = cv2.version
+    if version == '4.2.0':
         ctrs,_ = cv2.findContours(edges,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_TC89_L1)
-    elif cv_version == '3.2.0':
-        _,ctrs,_ = cv2.findContours(edges,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_TC89_L1)
+    else:
+        _, ctrs,_ = cv2.findContours(edges,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_TC89_L1)
     chosen_lst = []
     areas = []
     img_area = img.shape[0]*img.shape[1]
@@ -69,22 +56,16 @@ def secondCrop(img, show=True):
         x,y, w, h = cv2.boundingRect(ctr)
         roi_area = w*h
         non_max_sup = roi_area/img_area
-        if non_max_sup > SECOND_CROP_LOWLIM and non_max_sup <=SECOND_CROP_HIGHLIM:
+        if non_max_sup > 0.70 and non_max_sup <=0.90:
             chosen_lst.append([x,y,w,h])
             areas.append(roi_area)
     if len(chosen_lst) != 0:
         max_index = np.argmin(areas)
         x, y, w, h = chosen_lst[max_index]
         # cv2.rectangle(img,(x,y), (x+w, y+h), (90,0,255),2)
-        secondCrop = thresh[y:y+h,x:x+w]
-        # retimg = img[y:y+h,x:x+w]
+        secondCrop = img[y:y+h,x:x+w]
     else:
-        secondCrop = thresh
-        # retimg = img
-    if show == True:
-        cv2.imshow('canny',edges)
-        cv2.imshow('thresh', thresh)
-    # return retimg, secondCrop
+        secondCrop = img
     return secondCrop
 def auto_canny(image, sigma=0.33):
     # compute the median of the single channel pixel intensities
@@ -98,14 +79,60 @@ def auto_canny(image, sigma=0.33):
     # return the edged image
     return edged
 
+# def opencvReadPlate(img, characterRecognition, show=True):
+#     charList=[]
+#     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+#     thresh_inv = cv2.adaptiveThreshold(gray,200,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY_INV,39,1)
+#     # edges = auto_canny(thresh_inv, sigma=0.1)
+#     edges = thresh_inv
+#     ctrs, _ = cv2.findContours(edges.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_L1)
+#     sorted_ctrs = sorted(ctrs, key=lambda ctr: cv2.boundingRect(ctr)[0])
+#     img_area = img.shape[0]*img.shape[1]
+
+#     for i, ctr in enumerate(sorted_ctrs):
+#         x, y, w, h = cv2.boundingRect(ctr)
+#         roi_area = w*h
+#         non_max_sup = roi_area/img_area
+
+#         if((non_max_sup >= 0.015) and (non_max_sup < 0.09)):
+#             # if ((h>1.2*w) and (3*w>=h)):
+#             if(h > w):
+#                 char = img[y:y+h,x:x+w]
+#                 charList.append(cnnCharRecognition(char, characterRecognition))
+#                 cv2.rectangle(img,(x,y),( x + w, y + h ),(90,0,255),2)
+#     if(show == True):
+#         tmpImg = img.copy()
+#         imsize = tmpImg.shape
+#         w = imsize[1]
+#         h = imsize[0]
+#         if w < MIN_WIDTH or h < MIN_HEIGHT:
+#             w_scale = math.ceil(MIN_WIDTH / w)
+#             h_scale = math.ceil(MIN_HEIGHT / h)
+#             scale = max(w_scale, h_scale)
+#             w = w * scale
+#             h = h * scale
+#         if w > MAX_WIDTH or h > MAX_HEIGHT:
+#             w_scale = math.ceil(w / MAX_WIDTH)
+#             h_scale = math.ceil(h / MAX_HEIGHT)
+#             scale = min(w_scale, h_scale)
+#             w = math.floor(w / scale)
+#             h = math.floor(h / scale)
+#         tmpImg = cv2.resize(tmpImg, (w, h), interpolation=cv2.INTER_CUBIC)
+#         cv2.imshow('OpenCV character segmentation',tmpImg)
+#     licensePlate="".join(charList)
+#     return licensePlate
 
 def opencvReadPlate(img, characterRecognition, show=True):
     charList=[]
-    cv_version = cv2.__version__
-    if cv_version == '4.2.0':
-        ctrs, hier = cv2.findContours(img.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_L1)
-    elif cv_version == '3.2.0':
-        _, ctrs, hier = cv2.findContours(img.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_L1)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    thresh_inv = cv2.adaptiveThreshold(gray,200,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY_INV,39,1)
+    # edges = auto_canny(thresh_inv, sigma=0.1)
+    edges = thresh_inv
+    version = cv2.version
+    if version == '4.2.0':
+        ctrs, hier = cv2.findContours(edges.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_L1)
+    else:
+        _, ctrs, hier = cv2.findContours(edges.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_L1)
     # sorted_ctrs = sorted(ctrs, key=lambda ctr: cv2.boundingRect(ctr)[0])
     img_area = img.shape[0]*img.shape[1]
 
@@ -116,9 +143,12 @@ def opencvReadPlate(img, characterRecognition, show=True):
         roi_area = w*h
         non_max_sup = roi_area/img_area
 
-        if((non_max_sup >= SEGMENT_LOWLIM) and (non_max_sup < SEGMENT_HIGHLIM)):
+        if((non_max_sup >= 0.015) and (non_max_sup < 0.09)):
             # if ((h>1.2*w) and (3*w>=h)):
             if(h > w):
+                # char = img[y:y+h,x:x+w]
+                # charList.append(cnnCharRecognition(char, characterRecognition))
+                # cv2.rectangle(img,(x,y),( x + w, y + h ),(90,0,255),2)
                 index_chosen_lst.append(i)
                 tmp = [cv2.boundingRect(ctr), list(hier[0][i])]
                 chosen_lst.append(tmp)
@@ -157,9 +187,7 @@ def cnnCharRecognition(img,characterRecognition):
     21:'L', 22:'M', 23:'N', 24:'P', 25:'Q', 26:'R', 27:'S', 28:'1', 29:'U',
     30:'V', 31:'W', 32:'X', 33:'Y', 34:'Z'}
     # print(img.shape)
-    # blackAndWhiteChar=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # blackAndWhiteChar = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY_INV,39,1)
-    blackAndWhiteChar = img
+    blackAndWhiteChar=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # print(blackAndWhiteChar.shape)
     blackAndWhiteChar = cv2.resize(blackAndWhiteChar,(75,100))
     # image = blackAndWhiteChar.reshape((1, 100,75, 1))
